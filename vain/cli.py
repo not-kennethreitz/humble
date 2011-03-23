2085a207877f4775fbd29ed8359c6449b38b877f
Kenneth Reitz <me@kennethreitz.com>
2011-03-23 06:01:03 -0400

    basic framework

---
+++ vain/cli.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+vain.cli
+~~~~~~~~
+
+This module contains the CLI interface for Vain.
+
+"""
+
+from . import core
+from clint import args
+
+
+def start():
+    print args
